aecce043b79a3a09e637649777984ba9903962b7
hunterlr <hongshouzh@163.com>
2018-09-08 10:36:29 +0800

    还原

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,62 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-//		//$this->load->view('welcome_message');
-//        //采集某页面所有的图片
-//        //$data = QueryList::get('http://cms.querylist.cc/bizhi/453.html')->find('img')->attrs('src');
-//        //打印结果
-//        //print_r($data->all());
-//
-        $url = "http://wap.cnki.net/touch/usercenter/Account/Validator";
-//        //$url = "http://httpbin.org/post";
-        $ql = QueryList::getInstance();
-        $jar = new \GuzzleHttp\Cookie\CookieJar();
-//        dump($jar);
-//        $get = $ql->get('http://wap.cnki.net/touch/usercenter',[],[
-//            'cookies' => $jar
-//        ]);
-//        dump($jar);
-//
-//
-//
-//
-        $ql->post($url,[
-            'username' => 'sztsgzw',
-            'password' => '400231',
-            'keeppwd' => 'keepPwd',
-            'app:' => ''
-        ],[
-            'proxy' => '192.168.2.40:8888',
-            'cookies' => $jar
-        ]);
-
-        dump($jar);
-
-        dump($jar->toArray());
-        $jsonjar = json_encode($jar->toArray());
-
-        $jararr = json_decode($jsonjar);
-
-        //$jarout = $jar->fromArray($jararr);
-        $jar->clear();
-        dump($jar);
-        foreach ($jararr as $cookiearr)
-        {
-           $str = "";
-           foreach ($cookiearr as $key=>$value)
-           {
-               $str .= $key."=".$value.";";
-           }
-            $cookie =  $jar->SetCookie(GuzzleHttp\Cookie\SetCookie::fromString($str));
-           dump($cookie);
-           //$cookie =  GuzzleHttp\Cookie\SetCookie::fromString();
-        }
-
-        dump($jar);
-        $ql->get('http://wap.cnki.net/touch/usercenter/Zone/Index',[],['cookies' => $jar,'proxy' => '192.168.2.40:8888']);
-
-        echo $ql->getHtml();
-
+		$this->load->view('welcome_message');
 	}
 
 }
